878b3147f772a9754609d8156208b4ee51369a66
sunstep <73174517+sunstep@users.noreply.github.com>
2025-01-04 17:46:09 +0100

    Add wrapper for batches api endpoint and add repository implementation and interface and service.

--- lib/data_contracts/interfaces/point_interfaces.dart
+++ lib/data_contracts/interfaces/point_interfaces.dart
@@ -1,5 +1,5 @@
-import 'package:dawarich/data_contracts/data_transfer_objects/api/v1/points/response/api_point_dto.dart';
-import 'package:dawarich/data_contracts/data_transfer_objects/api/v1/points/response/slim_api_point_dto.dart';
+import 'package:dawarich/domain/data_transfer_objects/api/v1/points/response/api_point_dto.dart';
+import 'package:dawarich/domain/data_transfer_objects/api/v1/points/response/slim_api_point_dto.dart';
 import 'package:option_result/option_result.dart';
 
 abstract interface class IPointInterfaces {

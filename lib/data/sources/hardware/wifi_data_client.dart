7c4ff65830e6109a5d104109543604464b97a8aa
sunstep <73174517+sunstep@users.noreply.github.com>
2025-02-01 16:11:19 +0100

    Fix double quotes in network string.

--- lib/data/sources/hardware/wifi_data_client.dart
+++ lib/data/sources/hardware/wifi_data_client.dart
@@ -1,24 +1,31 @@
-
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 
 class WiFiDataClient {
 
   Future<String> getWiFiStatus() async {
-
     final List<ConnectivityResult> connectivityResults =
     await Connectivity().checkConnectivity();
 
     if (connectivityResults.contains(ConnectivityResult.wifi)) {
       try {
         final NetworkInfo wifiInfo = NetworkInfo();
-        final String? ssid = await wifiInfo.getWifiName();
-        return ssid ?? "Unknown";
+        final String? rawSSID = await wifiInfo.getWifiName();
+
+        // Clean the output by removing outer quotes.
+        final String ssid = (rawSSID != null &&
+            rawSSID.startsWith('"') &&
+            rawSSID.endsWith('"'))
+            ? rawSSID.substring(1, rawSSID.length - 1)
+            : rawSSID ?? "Unknown";
+        return ssid;
       } catch (e) {
         return "Unknown";
       }
+    } else if (connectivityResults.contains(ConnectivityResult.mobile)) {
+      return "Mobile Data";
     } else {
-      return "No Wi-Fi";
+      return "No Connectivity";
     }
   }
 }

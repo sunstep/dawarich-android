6bbad4f639cf86cdb12cfd86a450be1a7226daf5
sunstep <sunsteppz+github@gmail.com>
2025-07-07 13:44:47 +0200

    Ensure user session gets refreshed on startup

--- lib/features/auth/application/services/connect_service.dart
+++ lib/features/auth/application/services/connect_service.dart
@@ -1,16 +1,18 @@
+import 'package:dawarich/core/domain/models/user.dart';
+import 'package:dawarich/features/auth/application/user_converter.dart';
 import 'package:dawarich/features/auth/data_contracts/data_transfer_objects/users/user_dto.dart';
 import 'package:dawarich/core/network/configs/api_config_manager_interfaces.dart';
 import 'package:dawarich/features/auth/data_contracts/interfaces/connect_repository_interfaces.dart';
-import 'package:dawarich/features/auth/data_contracts/interfaces/user_storage_repository_interfaces.dart';
+import 'package:dawarich/features/auth/data_contracts/interfaces/user_repository_interfaces.dart';
 import 'package:flutter/foundation.dart';
 import 'package:option_result/option_result.dart';
-import 'package:user_session_manager/user_session_manager.dart';
+import 'package:session_box/session_box.dart';
 
 final class ConnectService {
   final IConnectRepository _connectRepository;
   final IApiConfigManager _apiConfigManager;
-  final IUserStorageRepository _userStorageRepository;
-  final UserSessionManager<int> _userSession;
+  final IUserRepository _userStorageRepository;
+  final SessionBox<User> _userSession;
 
   ConnectService(this._connectRepository, this._apiConfigManager,
       this._userStorageRepository, this._userSession);
@@ -37,9 +39,12 @@ final class ConnectService {
         await _connectRepository.loginApiKey(apiKey);
 
     if (loginResult case Ok(value: UserDto userDto)) {
-      final int userId = await _userStorageRepository.storeUser(userDto);
       await _apiConfigManager.storeApiConfig();
-      await _userSession.login(userId);
+      int userId = await _userStorageRepository.storeUser(userDto);
+      final User user = userDto.toDomain();
+      user.addUserId(userId);
+      await _userSession.login(user);
+      _userSession.setUserId(userId);
       return true;
     }
 
--- lib/features/migration/presentation/models/migration_viewmodel.dart
+++ lib/features/migration/presentation/models/migration_viewmodel.dart
@@ -1,8 +1,9 @@
 import 'package:dawarich/core/database/drift/database/sqlite_client.dart';
 import 'package:dawarich/core/di/dependency_injection.dart';
+import 'package:dawarich/core/domain/models/user.dart';
 import 'package:dawarich/core/routing/app_router.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:user_session_manager/user_session_manager.dart';
+import 'package:session_box/session_box.dart';
 
 final class MigrationViewModel extends ChangeNotifier {
   bool _isMigrating = false;
@@ -23,8 +24,8 @@ final class MigrationViewModel extends ChangeNotifier {
       await getIt<SQLiteClient>().customSelect('SELECT 1').get();
 
       // After migration, check login state
-      final sessionService = getIt<UserSessionManager<int>>();
-      final int? userId = await sessionService.getUser();
+      final sessionService = getIt<SessionBox<User>>();
+      final int? userId = sessionService.getUserId();
 
       if (context.mounted && userId != null && userId > 0) {
         Navigator.pushReplacementNamed(context, AppRouter.map);
